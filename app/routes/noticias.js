cca885d282afdb80e55c0a6d821fabb7dc63ddf6
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-09-20 10:46:26 -0300

    add models em noticias e inicio de envio de formularios

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -3,9 +3,11 @@ module.exports = function(app){
 	app.get('/noticias', function(req, res){
 
 		var connection = app.config.dbConnection();
+		var noticiasModel = app.app.models.noticiasModel;
 
-		connection.query('select * from noticias', function(error, result){			
+		noticiasModel.getNoticias(connection,function(error, result){			
 			res.render("noticias/noticias", {noticias : result});
 		});
+		
 	});
 };
